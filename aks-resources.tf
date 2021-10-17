d81032ac43cbfa37b589db59afaa18e1c3d66f89
thales <thales.claro@gmail.com>
2021-10-17 23:39:31 +0100

    module: aks deployment

--- aks-resources.tf
+++ aks-resources.tf
@@ -28,3 +28,21 @@ module "aks_role" {
   verbs      = ["get"]
 }
 
+module "aks_deployment" {
+  source          = "./modules/kubernetes/deployment"
+  for_each        = var.aks_resources.deployments
+  replicas        = each.value.replicas
+  node_label      = "agentpool"
+  node_values     = each.value.agentpool
+  label           = each.key
+  container_image = format("%s/%s:%s", each.value.docker_registry, each.value.docker_image, each.value.docker_tag)
+  container_name  = each.key
+  port_name       = "http"
+  container_port  = each.value.port
+  port_protocol   = "TCP"
+  limit_cpu       = each.value.resources.limit.cpu
+  limit_memory    = each.value.resources.limit.memory
+  request_cpu     = each.value.resources.requests.cpu
+  request_memory  = each.value.resources.requests.memory
+}
+
