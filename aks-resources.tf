d24c07292469027b9c9205857b14f13e7a004f3b
thales <thales.claro@gmail.com>
2021-10-17 23:13:16 +0100

    typo

--- aks-resources.tf
+++ aks-resources.tf
@@ -1,11 +1,11 @@
 module "aks_namespace" {
-  source   = "./modules/aks/namespace"
+  source   = "./modules/kubernetes/namespace"
   for_each = toset(var.aks_resources.namespaces)
   name     = each.key
 }
 
 module "aks_role_binding" {
-  source    = "./modules/aks/role_binding"
+  source    = "./modules/kubernetes/role_binding"
   for_each  = var.aks_resources.deployments
   name      = each.key
   namespace = each.value.namespace
